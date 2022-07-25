f75c4a17476bf3e4267bfafa64fbdeb9ee370b04
George Duncan <gtduncan@usc.edu>
2022-07-25 11:09:11 -0400

    commit tree c010715b54ab77e0f6f2864ab0d0afc08c612206

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,43 @@ import React from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
+import {useState} from 'react';
 
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [category, setCategory] = useState("All")
+
+  const onTaskFormSubmit = (newTask) =>
+  {
+    setTasks([...tasks, newTask]);
+  }
+
+  const onDeleteTask = (deleteTask) =>
+  {
+    setTasks(tasks.filter((task) => task.text !== deleteTask));
+  }
+
+  const taskstoDisplay = tasks.filter((task) => {
+    if(category==="All")
+    {
+      return true;
+    }
+    else
+    {
+      return task.category === category;
+    }
+  })
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selectedCategory={category} setCategory={setCategory}/>
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={taskstoDisplay} deleteTask={onDeleteTask}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, selectedCategory, setCategory}) {
+
+  const categoryButtons = categories.map((category) => {
+    const className = selectedCategory === category ? "selected" : null;
+    return <button onClick={() => setCategory(category)} key={category} className={className}>{category}</button>
+  })
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
-}
+  }
 
 export default CategoryFilter;
